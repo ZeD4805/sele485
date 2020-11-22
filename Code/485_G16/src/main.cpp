563338ccd2b7b578e4edc899ff992ed5b8abe5b1
ZeD4805 <zed4805@gmail.com>
2020-11-22 15:03:49 +0000

    Interrupts and Multi-processor mode

--- Code/485_G16/src/main.cpp
+++ Code/485_G16/src/main.cpp
@@ -1,32 +1,59 @@
-//#include <Arduino.h>
-
 #include <avr/io.h>
-//#include <avr/interrupt.h>
-//#include "USART.h"
+#include <avr/interrupt.h>
 
 #define UBRRH_VAL   ((F_CPU / (16 * baud)) - 1) >> 8
 #define UBRRL_VAL   ((F_CPU / (16 * baud)) - 1) & 0xff
 
 uint8_t address;
 
-void initUSART(uint32_t baud, uint8_t isTX) {
+ISR(USART_RX_vect){
+    uint16_t received = receiveData();
+    if(received & 8){
+        if(received == address)
+            UCSR0A &= ~(1 << MPCM0); //await data frame by disabling Multi-processor mode
+        
+    }
+    else
+    {
+        if(receiveData)
+            PORTB &= ~(1<<PB5);
+        else
+            PORTB |= (1<<PB5);
+
+        UCSR0A |= (1 << MPCM0); //reactivate Multi-processor mode
+    }
+    
+}
+
+void initUSART_ISR(uint32_t baud, uint8_t isTX) {
     UBRR0H = UBRRH_VAL;
     UBRR0L = UBRRL_VAL;
 
-    UCSR0A &= ~(1 << U2X0);
+    UCSR0B = (isTX << TXEN0) | (!isTX << RXEN0) | (!isTX << RXCIE0) | (1 << UPM00) | (1 << UPM01);//interrupt enable and parity
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 
-    UCSR0B = (isTX << TXEN0) | (!isTX << RXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00) | (1 << UPM00) | (1 << UPM01); // - parity
-}
+} 
 
-void transmitByte(uint8_t data) {
+void transmitData(uint16_t data) { 
     loop_until_bit_is_set(UCSR0A, UDRE0);
-    UDR0 = data;
+    UCSR0B &= ~(1<<TXB80);
+    if (data & 8)
+        UCSR0B |= (1<<TXB80);
+    UDR0 = (uint8_t) data;
 }
 
-uint8_t receiveByte(void) {
+uint16_t receiveData(void) {
     loop_until_bit_is_set(UCSR0A, RXC0);
-    return UDR0;
+    uint8_t status, resh, resl;
+
+    status = UCSR0A;
+    resh = UCSR0B;
+    resl = UDR0;
+    if (status & (1<<FE0)|(1<<DOR0)|(1<<UPE0))
+    return -1;
+
+    resh = (resh >> 1) & 0x01;
+    return ((resh << 8) | resl);
 }
 
 void setup(){
@@ -39,80 +66,47 @@ void setup(){
  
     address = PINB & (1 << PIN3 | 1 << PIN2 | 1 << PIN1 | 1 << PIN0);
 
-    initUSART(9600, !address);
-}
+    initUSART_ISR(9600, !address);
 
-void loop(){
-    if(!address){
-        PORTC |= !address << PC0; 
-        transmitByte(PINC & (1 << PC2 | 1 << PC1));
-        PORTC &= ~(!address << PC0); 
-    }   
-    else
-    {
-        uint8_t received = receiveByte();
-        switch (address)
-        {
-        case 1:
-            if(received & (1<<PC1)){
-                PORTB &= ~(1<<PB5);
-            }
-            else
-            {
-                PORTB |= (1<<PB5);
-            }
-            break;
-        case 2:
-            if(received & (1<<PC2)){
-                PORTB &= ~(1<<PB5);
-            }
-            else
-            {
-                PORTB |= (1<<PB5);
-            }
-        default:
-            break;
-        }
-    }
-}
-
-void masterLoop(){
-    PORTC |= !address << PC0; 
-    transmitByte(PINC & (1 << PC2 | 1 << PC1));
-    PORTC &= ~(!address << PC0); 
+    sei();
 }
 
 void slaveLoop(){
-    uint8_t received = receiveByte();
-    switch (address)
-    {
-    case 1:
-        if(received & (1<<PC1)){
-            PORTB &= ~(1<<PB5);
-        }
-        else
-        {
-            PORTB |= (1<<PB5);
-        }
-        break;
-    case 2:
-        if(received & (1<<PC2)){
-            PORTB &= ~(1<<PB5);
-        }
-        else
-        {
-            PORTB |= (1<<PB5);
-        }
-    default:
-        break;
+
+    while(1);
+}
+
+void masterLoop(){
+    uint8_t buttonStatus, prevButtonStatus = 0;
+    PINC & (1 << PC2 | 1 << PC1);
+
+    while(1){
+        buttonStatus = PINC & (1 << PC2 | 1 << PC1);
+        if(buttonStatus != prevButtonStatus){
+            if((buttonStatus&1) != (prevButtonStatus&1)){ //address 1
+                PORTC |= !address << PC0; 
+                transmitData(8 + (buttonStatus & 1));
+                transmitData(buttonStatus & 1);
+                PORTC &= ~(!address << PC0); 
+            }
+            else if((buttonStatus&2) != (prevButtonStatus&2)){ //address 2
+                PORTC |= !address << PC0; 
+                transmitData(8 + (buttonStatus&2));
+                transmitData(buttonStatus & 2);
+                PORTC &= ~(!address << PC0); 
+            }
+        } 
     }
 }
 
-int main(){
+void main(){
     setup();
-
-    if(!address) while (1) masterLoop();
-    else while(1) slaveLoop();
-
-    return 0;
+    if(!address){
+        masterLoop();
+    }
+    else
+    {
+        slaveLoop();
+    }
+    
 }

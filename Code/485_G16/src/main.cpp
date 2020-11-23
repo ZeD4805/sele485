4bcdb7fd9210b4813f7858bb91ec361b1e28f917
ZeD4805 <zed4805@gmail.com>
2020-11-23 23:54:28 +0000

    last minute fix

--- Code/485_G16/src/main.cpp
+++ Code/485_G16/src/main.cpp
@@ -71,8 +71,7 @@ void slaveLoop(){
             }
             else
             {
-                UCSR0A &= ~(1 << MPCM0); //make sure it looks for address frame
-                UCSR0A |= (1 << MPCM0);
+                UCSR0A |= (1 << MPCM0); //make sure it looks for address frame
             }
         }
         else // if dataFrame
